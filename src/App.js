bff27ed6d2ef011cb5aea3652722227a512f0f26
manoj bisht <manoj1.bisht@paytm.com>
2022-01-14 20:31:45 +0530

    router set up demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
+import {Route, Switch, Link} from 'react-router-dom';
 
+const Homepage1 = (props) =>{
+  console.log(props);
+  return(
+    
+    <div>
+    <button onClick={() => (props.history.push('/topics'))}>Topics</button>
+    Homepage
+    </div>
+  )
+}
+
+const Topics = (props) =>(
+  <div>
+  <Link to={`${props.match.url}/13`}>Topics</Link>
+  Topic Page</div>
+)
+const TopicsDetails = (props) =>{
+  console.log(props);
+  return(
+  <div>Topics Details Page {props.match.params.topicId}</div>
+  )
+}
 
 function App() {
   return (
     <div >
-      <Homepage />
+    <Switch>
+      <Route exact path='/' component={Homepage1} />
+      <Route exact path='/topics' component={Topics} />
+      <Route  path='/topics/:topicId' component={TopicsDetails} />
+    </Switch>
     </div>
   );
 }
